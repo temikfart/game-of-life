574fbf35e1669b9dddef12c8e736d17e00281e4a
Artyom Fartygin <fartygin.a@phystech.edu>
2024-04-19 17:37:11 +0300

    Hide generation type in class

--- include/game_of_life.hpp
+++ include/game_of_life.hpp
@@ -17,11 +17,24 @@ constexpr int kFrequency = 3;
 
 class GameOfLife {
 public:
-    struct Cell {
+    class Generation {
     public:
-        bool alive = false;
-    };
+        struct Cell {
+            bool alive = false;
+        };
 
+        int width;
+        int height;
+
+        explicit Generation(int width, int height);
+
+        const Cell& cell(int x, int y) const;
+        void setState(int x, int y, bool alive);
+        void setRandomStates();
+
+    private:
+        std::vector<std::vector<Cell>> gen_;
+    };
 
     GameOfLife();
 
@@ -29,22 +42,22 @@ public:
 
 private:
     Window window_;
-    std::vector<std::vector<Cell>> curr_gen_;
-    std::vector<std::vector<Cell>> next_gen_;
+    Generation curr_gen_;
+    Generation next_gen_;
 
     void calcNextGen();
-    bool isCellAliveInCurrGen(int x, int y);
-    int calcAliveNeighborCount(int x, int y);
-    inline bool underpopulation(int alive_neighbor_count) {
+    bool isCellAliveInCurrGen(int x, int y) const;
+    int calcAliveNeighborCount(int x, int y) const;
+    static inline bool underpopulation(int alive_neighbor_count) {
         return alive_neighbor_count < 2;
     }
-    inline bool nextGeneration(int alive_neighbor_count) {
+    static inline bool nextGeneration(int alive_neighbor_count) {
         return alive_neighbor_count == 2 || alive_neighbor_count == 3;
     }
-    inline bool overpopulation(int alive_neighbor_count) {
+    static inline bool overpopulation(int alive_neighbor_count) {
         return alive_neighbor_count > 3;
     }
-    inline bool reproduction(int alive_neighbor_count) {
+    static inline bool reproduction(int alive_neighbor_count) {
         return alive_neighbor_count == 3;
     }
     void drawNextGen();
--- src/game_of_life.cpp
+++ src/game_of_life.cpp
@@ -2,15 +2,31 @@
 
 namespace gol {
 
-GameOfLife::GameOfLife()
-    : window_(kWidth, kHeight, kCellSize),
-      curr_gen_(kHeight, std::vector<Cell>(kWidth)),
-      next_gen_(kHeight, std::vector<Cell>(kWidth)) {
-    for (auto& line : curr_gen_) {
+using Generation = GameOfLife::Generation;
+using Cell = GameOfLife::Generation::Cell;
+
+GameOfLife::Generation::Generation(int width, int height)
+    : width(width), height(height), gen_(height, std::vector<Cell>(width)) {}
+
+const Cell& Generation::cell(int x, int y) const {
+    return gen_[y][x];
+}
+void Generation::setState(int x, int y, bool alive) {
+    gen_[y][x].alive = alive;
+}
+void Generation::setRandomStates() {
+    for (auto& line : gen_) {
         for (auto& cell : line) {
             cell.alive = (utils::randInt() % kFrequency) == 0;
         }
     }
+}
+
+GameOfLife::GameOfLife()
+    : window_(kWidth, kHeight, kCellSize),
+      curr_gen_(kWidth, kHeight),
+      next_gen_(kWidth, kHeight) {
+    curr_gen_.setRandomStates();
 }
 
 void GameOfLife::run() {
@@ -22,16 +38,15 @@ void GameOfLife::run() {
     }
 }
 void GameOfLife::calcNextGen() {
-    for (int y = 0; y < kHeight; y++) {
-        for (int x = 0; x < kWidth; x++) {
-            next_gen_[y][x].alive = isCellAliveInCurrGen(x, y);
+    for (int y = 0; y < next_gen_.height; y++) {
+        for (int x = 0; x < next_gen_.width; x++) {
+            next_gen_.setState(x, y, isCellAliveInCurrGen(x, y));
         }
     }
 }
-bool GameOfLife::isCellAliveInCurrGen(int x, int y) {
-    Cell& cell = curr_gen_[y][x];
+bool GameOfLife::isCellAliveInCurrGen(int x, int y) const {
     int alive_neighbors_count = calcAliveNeighborCount(x, y);
-    if (cell.alive) {
+    if (curr_gen_.cell(x, y).alive) {
         if (underpopulation(alive_neighbors_count) || overpopulation(alive_neighbors_count)) {
             return false;
         }
@@ -41,7 +56,7 @@ bool GameOfLife::isCellAliveInCurrGen(int x, int y) {
     }
     return reproduction(alive_neighbors_count);
 }
-int GameOfLife::calcAliveNeighborCount(int x, int y) {
+int GameOfLife::calcAliveNeighborCount(int x, int y) const {
     int count = 0;
     for (int dx = -1; dx <= 1; dx++) {
         for (int dy = -1; dy <= 1; dy++) {
@@ -51,7 +66,7 @@ int GameOfLife::calcAliveNeighborCount(int x, int y) {
             int nx = x + dx;
             int ny = y + dy;
             if (nx >= 0 && nx < kWidth && ny >= 0 && ny < kHeight) {
-                if (curr_gen_[ny][nx].alive) {
+                if (curr_gen_.cell(nx, ny).alive) {
                     count++;
                 }
             }
@@ -62,7 +77,7 @@ int GameOfLife::calcAliveNeighborCount(int x, int y) {
 void GameOfLife::drawNextGen() {
     for (int y = 0; y < kHeight; y++) {
         for (int x = 0; x < kWidth; x++) {
-            if (next_gen_[y][x].alive) {
+            if (next_gen_.cell(x, y).alive) {
                 window_.paintCell(x, y);
             }
         }
